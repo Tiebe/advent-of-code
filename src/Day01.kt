5521fbfb550374f5e8fa02732407a90cd0fcf263
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-17 15:19:08 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): MutableList<Int> {
+        val sumList = mutableListOf(0)
+        var index = 0
+
+        input.forEach {
+            if (it == "") { index++; sumList.add(0) }
+            else {
+                sumList[index] += it.toInt()
+            }
+        }
+
+        return sumList
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sumList = part1(input)
+
+        var sum = 0
+
+        repeat(3) {
+            sum += sumList.maxOrNull() ?: 0
+            sumList.remove(sumList.maxOrNull() ?: 0)
+        }
+
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check((part1(testInput).maxOrNull() ?: 0) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
-    part1(input).println()
+    (part1(input).maxOrNull() ?: 0).println()
     part2(input).println()
 }
