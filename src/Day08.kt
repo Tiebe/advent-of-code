a68ae45d776467ab3293fa2dbd62c0e2ef843d16
Tiebe Groosman <tiebe.groosman@gmail.com>
2022-12-18 21:10:15 +0100

    day 8 thats much better

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,121 +1,67 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var visibleCount = input[0].length * 2 + input.size * 2 - 4
+    fun List<String>.toMatrix(): Array<IntArray> = this.map { line -> line.map { it.digitToInt() }.toIntArray() }.toTypedArray()
 
-        for (treesIndex in input.subList(1, input.size - 1).indices) {
-            val trees = input[treesIndex+1]
+    fun Array<IntArray>.getTopVisibility(x: Int, y: Int): Int {
+        val pointValue = this[y][x]
 
-            treeLoop@ for (treeSelectIndex in trees.indices) {
-                if (treeSelectIndex == 0 || treeSelectIndex == trees.length-1) continue@treeLoop
-
-                //check left
-                if (trees.subSequence(0, treeSelectIndex).all {
-                    it < trees[treeSelectIndex]
-                }) visibleCount++
-
-                // check right
-                else if (trees.subSequence(treeSelectIndex+1, trees.length).all {
-                    it < trees[treeSelectIndex]
-                }) visibleCount++
-
-                //check top
-                else if (input.subList(0, treesIndex+1).all {
-                        it[treeSelectIndex] < trees[treeSelectIndex]
-                }) visibleCount++
-
-                // check bottom
-                else if (input.subList(treesIndex+2, input.size).all {
-                        it[treeSelectIndex] < trees[treeSelectIndex]
-                }) visibleCount++
+        for (i in y-1 downTo 0) {
+            if (this[i][x] >= pointValue) {
+                return y-i-1
             }
         }
 
-        return visibleCount
+        return y
+    }
+
+    fun Array<IntArray>.getBottomVisibility(x: Int, y: Int): Int {
+        val pointValue = this[y][x]
+
+        for (i in y+1 until size) {
+            if (this[i][x] >= pointValue) {
+                return i-y
+            }
+        }
+
+        return size-y-1
+    }
+
+    fun Array<IntArray>.getLeftVisibility(x: Int, y: Int): Int {
+        val pointValue = this[y][x]
+
+        for (i in x-1 downTo 0) {
+            if (this[y][i] >= pointValue) {
+                return x-i
+            }
+        }
+
+        return x
+    }
+
+    fun Array<IntArray>.getRightVisibility(x: Int, y: Int): Int {
+        val pointValue = this[y][x]
+
+        for (i in x+1 until size) {
+            if (this[y][i] >= pointValue) {
+                return i-x
+            }
+        }
+
+        return size-x-1
+    }
+
+    fun part1(input: List<String>): Int {
+        val matrix = input.toMatrix()
+
+        matrix.forEach { it.forEach(::print); println() }
+
+        matrix.getRightVisibility(1, 0).println()
+        matrix.getRightVisibility(1, 2).println()
+
+        return 0
     }
 
     fun part2(input: List<String>): Int {
-        var highestScore = 0
-
-        for (treesIndex in input.indices) {
-            val trees = input[treesIndex]
-
-            treeLoop@ for (treeSelectIndex in trees.indices) {
-                if (treesIndex != 3) continue@treeLoop
-                if (treeSelectIndex != 2) continue@treeLoop
-
-                var score = 1
-
-                var starting = score
-                //check left
-                for (i in 0 until treeSelectIndex) {
-                    val check = treeSelectIndex - i - 1
-
-                    if (trees[check] >= trees[treeSelectIndex]) {
-                        score *= i
-
-                        println("$trees: ${trees[check]}: score: $i")
-                        break
-                    }
-                }
-
-                if (starting == score) {
-                    score *= treeSelectIndex
-                }
-
-                starting = score
-                // check right
-                for (i in (treeSelectIndex+1) until trees.length) {
-                    if (trees[i] >= trees[treeSelectIndex]) {
-                        score *= (i-treeSelectIndex)
-
-                        break
-                    }
-                }
-
-                if (starting == score) {
-                    score *= (trees.length - treeSelectIndex)
-                }
-
-                starting = score
-
-                // check top
-                for (i in 0 until treesIndex) {
-                    val check = treesIndex - i - 1
-
-                    if (input[check][treeSelectIndex] >= trees[treeSelectIndex]) {
-                        score *= (i+1)
-                    }
-                }
-
-                if (starting == score) {
-                    score *= (input.size - treesIndex)
-                }
-
-                starting = score
-
-                for (i in (treesIndex+1) until input.size) {
-                    if (input[i][treeSelectIndex] >= trees[treeSelectIndex]) {
-                        score *= (i-treesIndex)
-                    }
-                }
-
-                if (starting == score) {
-                    score *= (input.size - treesIndex)
-                }
-
-
-                println(score)
-                if (score > highestScore) highestScore = score
-
-
-            }
-        }
-
-        println(highestScore)
-
-
-
-        return highestScore
+        return input.size
     }
 
     // test if implementation meets criteria from the description, like:
